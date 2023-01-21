c85d606d2c1e171928d8f7f564d3583cfd168ac7
SleepingShell <SleepingShell@users.noreply.github.com>
2023-01-20 22:14:34 -0600

    Initialize

---
+++ README.md
@@ -0,0 +1,7 @@
+# zk-ERC20
+This is an implementation of shielded UTXO transfers I created to teach myself Circom. Design is heavily inspried by TC while I have also studied RAILGUN.
+
+The [SHIELD framework](https://github.com/xorddotcom/SHIELD) sets up an easy environment for circuit compilation and testing.
+
+# Deposit
+A user deposits by creating a transaction with 0-value commitments.
---
+++ shield.config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  // (optional) solidity version for compiled contracts, defaults to `^0.8.0`
+  solidity: "^0.8.0",
+  circom: {
+    // (optional) Base path for files being read, defaults to `/circuits`
+    inputBasePath: "/circuits",
+    // (optional) Base path for files being output, defaults to `/build`
+    outputBasePath: "/build",
+    // (required) The final ptau file, relative to inputBasePath, from a Phase 1 ceremony
+    ptau: "powersOfTau28_hez_final_14.ptau",
+    // (required) Each object in this array refers to a separate circuit
+    circuits: [
+      /*
+      {
+        // (required) The name of the circuit
+        name: "MerkleTree",
+        // (required) Protocol used to build circuits ("groth16" or "plonk"), defaults to "groth16"
+        protocol: "plonk",
+        // (required) Input path for circuit file, inferred from `name` if unspecified
+        circuit: "MerkleTree.circom",
+        // (required) Output path for zkey file, inferred from `name` if unspecified
+        zkey: "MerkleTree.zkey",
+        // (optional) Input path for input signal data, inferred from `name` if unspecified
+        input: "input.json",
+        // // (optional) Output path for witness file, inferred from `name` if unspecified
+        witness: "MerkleTree/MerkleTree.json",
+      },
+      */
+      {
+        name: "MakeProof",
+        protocol: "plonk",
+        circuit: "test/MakeProof.circom",
+        zkey: "MakeProof.zkey",
+        input: "input.json",
+        witness: "MakeProof/MakeProof.json",
+      },
+      {
+        name: "VerifyProof",
+        protocol: "plonk",
+        circuit: "test/VerifyProof.circom",
+        zkey: "VerifyProof.zkey",
+        input: "input.json",
+        witness: "VerifyProof/VerifyProof.json",
+      },
+      {
+        name: "Transaction",
+        protocol: "plonk",
+        circuit: "Transaction.circom",
+        input: "input.json",
+        witness: "Transaction/Transaction.json"
+      }
+    ],
+  },
+};
