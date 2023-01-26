11e0184dcc932d03942e26f9fa4e1b76f17d8a11
SleepingShell <SleepingShell@users.noreply.github.com>
2023-01-25 23:55:08 -0600

    Update typechain output directory

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,18 @@
-import { HardhatUserConfig } from 'hardhat/config';
-import '@nomicfoundation/hardhat-toolbox';
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.9',
+    version: "0.8.9",
     settings: {
       optimizer: {
         enabled: true,
         runs: 1000,
       },
     },
+  },
+  typechain: {
+    outDir: "./types",
   },
 };
 
